9b1981d3ab112bc8cdb5f69f66f24ba5847cc692
nitinkumar593 <nitinkumar.in593@gmail.com>
2026-05-03 17:22:07 +0530

    do some correction

--- app.js
+++ app.js
@@ -22,81 +22,82 @@ async function main() {
     await mongoose.connect(process.env.MONGO_URL);
 }
 app.set("view engine ", "ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended:true}));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
     res.send("Port is working");
 });
 
+// middleware for validating the listing data using Joi
 const validateListing = (req, res, next) => {
 
     const { error } = listingSchema.validate(req.body);
-    if(error){
+    if (error) {
         let errMessage = error.details.map(el => el.message).join(","); // this will give us all the error messages in a single string
-        throw new ExpressError(400,errMessage);
-    }else{
-    next();
+        throw new ExpressError(400, errMessage);
+    } else {
+        next();
     }
-};  
+};
 
 // index route
-app.get("/listings",wrapAsync( async (req,res)=>{
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListing = await Listing.find({});
-    res.render("listings/index.ejs",{allListing});
+    res.render("listings/index.ejs", { allListing });
 }));
 
 // New and Create route
-app.get("/listings/new" , (req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 
-app.post("/listings", validateListing, wrapAsync(async (req,res)=>{
+app.post("/listings", validateListing, wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 }));
 
 // show route
-app.get("/listings/:id",wrapAsync( async (req,res)=>{
-    const {id} = req.params;
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    const { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
+    res.render("listings/show.ejs", { listing });
 }));
 
 // Edit route
-app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
-    const {id} = req.params;
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+    const { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
+    res.render("listings/edit.ejs", { listing });
 }));
 
 // Update route
-app.put("/listings/:id", validateListing, wrapAsync(async (req,res)=>{
-    const {id} = req.params;    
-    await Listing.findByIdAndUpdate(id, {...req.body.listing},{ new: true, runValidators: true });//in this i'm using spread operator (...) because it helps directly matches with schema
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing }, { new: true, runValidators: true });//in this i'm using spread operator (...) because it helps directly matches with schema
     res.redirect(`/listings/${id}`);
 }));
 
 // delete route
-app.delete("/listings/:id",wrapAsync( async (req,res)=> {
-    const {id} = req.params;
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    const { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
 }));
 
 // handling all other routes which are not defined
-app.all("*", (req,res,next)=>{
-    next(new ExpressError(404,"Page Not Found"));
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
 });
 
 // error handling middleware
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).render("error.ejs", {message});
+    res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(8080, () => {
